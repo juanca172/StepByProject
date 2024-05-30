dc8d227f3e1fb9914493f1fe4ed5c50cfb2fe017
juanca172 <61369165+juanca172@users.noreply.github.com>
2024-05-30 12:59:44 -0500

    Initial Commit

---
+++ StepByProject/StepByProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepByProjectApp.swift
+//  StepByProject
+//
+//  Created by Juan Camilo Fonseca Gomez on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StepByProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
